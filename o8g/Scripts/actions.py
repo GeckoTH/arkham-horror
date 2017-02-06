a90dec1d6064728659f898dab192ab095b91dca9
rushl <rushl@palmtreebeach.net>
2017-02-06 12:01:10 -0600

    Adding path markers!

--- o8g/Scripts/actions.py
+++ o8g/Scripts/actions.py
@@ -123,6 +123,8 @@ def cardDoubleClicked(args):
                 doDiscard(me, card, chaosBag())
             else:
                 remoteCall(card.controller, "doDiscard", [me, card, chaosBag()])
+        elif card.Type == "Path": # Rotate Path cards
+            rotateRight(card)
 
 def activePlayers():
     count=0
@@ -930,6 +932,9 @@ def defaultAction(card, x = 0, y = 0):
     # Default for Done button is playerDone
     if not card.isFaceUp: #Face down card - flip
         flipcard(card, x, y)
+    elif card.Type == "Path": # Action handled in OnCardDoubleClicked
+        # Do nothing
+        mute()
     elif card.orientation & Rot90 == Rot90: #Rotated card - refresh
         exhaust(card, x, y)
     elif card.Type == "Agenda":
@@ -1506,6 +1511,53 @@ def drawBasicWeaknessToHand(group, x = 0, y = 0):
     card = drawBasicWeakness(group, x, y)
     card.moveTo(me.hand)
     notify("{} draws the Basic Weakness '{}' into their hand.".format(me, card))
+
+def placeLongPath(group, x=0, y=0):
+    pathCard = group.create("7f4029c8-1cee-406a-9913-9fbc6e341bed", x, y, 1, False)
+    pathCard.sendToBack()
+
+def placeMediumPath(group, x=0, y=0):
+    pathCard = group.create("cf3d8bd6-354a-4284-b716-109e7040c3e9", x, y, 1, False)
+    pathCard.sendToBack()
+
+def placeShortPath(group, x=0, y=0):
+    pathCard = group.create("2e964666-fa5a-40e4-a7f5-bf66c625d783", x, y, 1, False)
+    pathCard.sendToBack()
+
+def placeElbowPath(group, x=0, y=0):
+    pathCard = group.create("3d9c7266-d4d0-46e0-b8b3-560fbcf1b294", x, y, 1, False)
+    pathCard.sendToBack()
+
+def placeCrossPath(group, x=0, y=0):
+    pathCard = group.create("8ea6845b-b9bb-4f11-a814-e94b16e50629", x, y, 1, False)
+    pathCard.sendToBack()
+
+def placeThreeWayPath(group, x=0, y=0):
+    pathCard = group.create("1b1493eb-cf9f-4709-9b50-f8f343f7a607", x, y, 1, False)
+    pathCard.sendToBack()
+
+def placeDiagonalConnectionPath(group, x=0, y=0):
+    pathCard = group.create("d2ddabd3-b7b1-427e-8ca2-b7dbe272fce5", x, y, 1, False)
+    pathCard.sendToBack()
+
+def placeDirectionalMarker(group, x=0, y=0):
+    pathCard = group.create("10bd7039-10f4-44c9-8be4-61bf182e1d9d", x, y, 1, False)
+    pathCard.sendToBack()
+
+def lockAllPaths(group, x=0, y=0):
+    for card in table:
+        if card.Type == "Path":
+            if not hasattr(card, 'Subtype'):
+                card.sendToBack()
+            card.anchor = True
+
+def unlockAllPaths(group, x=0, y=0):
+    for card in table:
+        if card.Type == "Path":
+            card.anchor = False    
+        
+
+
 
 # def captureDeck(group):
 #   if len(group) == 0: return
