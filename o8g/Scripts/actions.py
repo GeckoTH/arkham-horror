dd9654fb288e1a81ba5298ebe994362ad009db6d
Pilunte <vad23@msn.com>
2020-12-15 19:01:53 +0100

    Update actions.py
    fix

--- o8g/Scripts/actions.py
+++ o8g/Scripts/actions.py
@@ -1347,7 +1347,7 @@ def markerChanged(args):
     if getGlobalVariable("phase") == "Mythos" or thisPhase[1] == 1:
         inMythosPhase = True
     
-    if card.Type == "Agenda" and args.marker == Doom[0] and inMythosPhase == True and card.properties[Doom[0]] != "":
+    if card.Type == "Agenda" and args.marker == Doom[0] and inMythosPhase == True and card.properties[Doom[0]] not in ["","-","X"]:
         maxDoom = int(card.properties[Doom[0]])
         totalDoom = 0
         for cardT in table:
