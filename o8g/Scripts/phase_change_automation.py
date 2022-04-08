d4625f74b8862b80c0b941259ec515b83fa68d69
Kwaice <kwaice@gmail.com>
2022-04-08 13:38:10 +0200

    Update phase_change_automation.py

--- o8g/Scripts/phase_change_automation.py
+++ o8g/Scripts/phase_change_automation.py
@@ -12,7 +12,7 @@ def doMythosPhase(setPhaseVar = True):
         setGlobalVariable("phase", "Mythos")
 
     for card in table:
-        if card.Name == "Sister Mary" and card.Type == "Investigator" and card.owner == me and blessInCB() < 10:
+        if card.Name == "Sister Mary" and card.Type == "Investigator" and card.owner == me and countBless() < 10:
             if 1 == askChoice('Add a Bless Token in the Chaos Bag ?', ['Yes', 'No'], ['#dd3737', '#d0d0d0']):
                 addBless()
     # Auto-replenish
