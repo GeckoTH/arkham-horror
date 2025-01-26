675e91aa8abc0c85f3dbe08d8bc9a78df76c68c2
ricardojm <ricardo_jm56@hotmail.com>
2025-01-26 04:32:40 -0300

    Update actions.py

--- o8g/Scripts/actions.py
+++ o8g/Scripts/actions.py
@@ -1875,7 +1875,7 @@ def drawAddChaosToken(group, x = 0, y = 0):
 def removeChaosTokenFromBag(card):
     if chaosBag().controller == me:
         for cT in chaosBag():
-            if cT.Name != card.Name:
+            if cT.Name != card:
                 continue
             cT.delete()
             break
